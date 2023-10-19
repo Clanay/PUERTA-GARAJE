7782faf9e65259f4cece356c9f0fc134d143dac5
Clanay <samuel.call.ramos@gmail.com>
2023-10-19 15:44:59 +0200

    Botones up y down acabados leds

--- Botones_y_down_con_leds/Botones_y_down_con_leds.ino
+++ Botones_y_down_con_leds/Botones_y_down_con_leds.ino
@@ -1,29 +1,34 @@
-const int pulsador_up=2;
-const int pulsador_down=3;
-const int ledrojo=10;
-const int ledverde=13;
+const int pulsador_up = 2;
+const int pulsador_down = 3;
+const int ledrojo = 10;
+const int ledverde = 13;
+int estado_pulsador_up=0;
+int estado_pulsador_down=0;
 
 void setup()
 {
-  pinMode(ledverde,OUTPUT);
-  pinMode(ledrojo,OUTPUT);
-  pinMode(pulsador_up,INPUT);
-  pinMode(pulsador_down,INPUT);
+  pinMode(ledverde, OUTPUT);
+  pinMode(ledrojo, OUTPUT);
+  pinMode(pulsador_up, INPUT);
+  pinMode(pulsador_down, INPUT);
+  estado_pulsador_up=0;
+  estado_pulsador_down=0;
 }
-void loop()
+void loop() 
 {
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(13,LOW);
-  delay(1000);
-}
-void loop()
-{
-  if(digitalRead(2)){
-      digitalWrite(13,HIGH);
+  estado_pulsador_up=digitalRead(pulsador_up);
+  estado_pulsador_down=digitalRead(pulsador_down);
+  if(estado_pulsador_up==1)
+  {
+    digitalWrite(ledverde,HIGH);
+    digitalWrite(ledrojo,LOW);
   }
-  else{
-  digitalWrite(13,LOW);
+  if(estado_pulsador_down==1)
+  {
+    digitalWrite(ledverde,LOW);
+    digitalWrite(ledrojo,HIGH); 
   }
-  delay(100);
 }
+
+
+
